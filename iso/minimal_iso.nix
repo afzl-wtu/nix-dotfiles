c95ae38d7e42937d4644a3f8c3d21b86cfc77392
Afzal Tahir Wattu <afzl.wtu@hotmail.com>
2025-07-06 14:02:09 +0500

    iso updated

--- iso/minimal_iso.nix
+++ iso/minimal_iso.nix
@@ -16,6 +16,9 @@
     fastfetch
     eza
     ripgrep
+    tmux
+    meslo-lgs-nf
+    fishPlugins.tide
   ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -27,6 +30,7 @@
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
+      tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Round --powerline_prompt_heads=Slanted --powerline_prompt_tails=Slanted --powerline_prompt_style='Two lines, character and frame' --prompt_connection=Dotted --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Darkest --prompt_spacing=Sparse --icons='Many icons' --transient=No
       set fish_greeting # Disable greeting
       fastfetch --config examples/13
       zoxide init fish | source
@@ -37,6 +41,15 @@
     "tree" = "eza --icons -T";
   };
     };
+  programs.bash = {
+    initExtra = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
 
   isoImage.squashfsCompression = "gzip -Xcompression-level 1";
 }
